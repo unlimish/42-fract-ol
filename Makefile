ea9763df2465d3cbd41684048b54a57149eff864
unlimish <mail@unlimish.xyz>
2021-08-14 14:07:52 +0900

    ♻️ Add(initialize.c): devide to new file initialize structure

--- Makefile
+++ Makefile
@@ -11,11 +11,12 @@ LIBFT_DIR = libft
 LIBFT_MAKE = make -C $(LIBFT_DIR)
 LIBFT_PREF = -L./libft -lft
 
-SRCS = main.c
+SRCS = main.c \
+		initialize.c
 OBJ = $(SRCS:.c=.o)
 
 mac:
-	$(CC) -L $(MLX_MAC_DIR) $(MLX_MAC_DIR)/libmlx.a $(MLX_MAC_DIR)/mlx.h src/main.c -framework OpenGL -framework AppKit && ./a.out
+	$(CC) -L $(MLX_MAC_DIR) $(MLX_MAC_DIR)/libmlx.a -I inc/ $(MLX_MAC_DIR)/mlx.h src/*.c -framework OpenGL -framework AppKit && ./a.out
 
 all: ${NAME}
 
---
+++ src/initialize.c
@@ -0,0 +1,15 @@
+#include "../inc/fractol.h"
+
+void init_prop(t_draw_prop *dr_p)
+{
+	dr_p->mlx = mlx_init();
+	dr_p->mlx_win = mlx_new_window(dr_p->mlx, WIDTH, HEIGHT, "fract-ol");
+
+	dr_p->img.img = mlx_new_image(dr_p->mlx, WIDTH, HEIGHT);
+	dr_p->img.addr = mlx_get_data_addr(dr_p->img.img, &dr_p->img.bit_per_px,
+									&dr_p->img.line_len, &dr_p->img.endian);
+	dr_p->imag_max = 2;
+	dr_p->real_max = 2;
+	dr_p->imag_min = -2;
+	dr_p->real_min = -2;
+}
